1051b8210ac24bfa95ec515eb03f7524498aaf84
Mina Rezkalla <minashakeralfy@gmail.com>
2020-11-10 19:45:28 +0100

    initial commit

---
+++ app/src/main/java/com/rezkalla/vehiclesfinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rezkalla.vehiclesfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
