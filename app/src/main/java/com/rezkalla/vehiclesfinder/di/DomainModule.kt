e278589e6a2458eb6c80af7a851051a71a9098d0
Mina Rezkalla <minashakeralfy@gmail.com>
2020-11-12 18:42:46 +0100

    add domain module

---
+++ app/src/main/java/com/rezkalla/vehiclesfinder/di/DomainModule.kt
@@ -0,0 +1,29 @@
+package com.rezkalla.vehiclesfinder.di
+
+import com.rezkalla.domain.qualifier.Background
+import com.rezkalla.domain.qualifier.Foreground
+import dagger.Module
+import dagger.Provides
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+import javax.inject.Singleton
+
+@Module
+class DomainModule {
+
+    @Singleton
+    @Provides
+    @Background
+    fun providesBackgroundScheduler(): Scheduler {
+        return Schedulers.io()
+    }
+
+    @Singleton
+    @Provides
+    @Foreground
+    fun providesForegroundScheduler(): Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+}
